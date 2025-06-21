a3208b70276def5ab7c1a124e6e3445b3ed66a2a
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2025-06-21 13:26:24 +0700

    menghapus beberapa baris code yang tidak di perlukan

--- app/src/main/java/com/uti/hidroponikapp/LoginActivity.kt
+++ app/src/main/java/com/uti/hidroponikapp/LoginActivity.kt
@@ -11,33 +11,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-//        val usernameEditText = findViewById<EditText>(R.id.etUsername)
-//        val passwordEditText = findViewById<EditText>(R.id.etPassword)
-//        val loginButton = findViewById<Button>(R.id.btnLogin)
-//
-//        loginButton.setOnClickListener {
-//            val username = usernameEditText.text.toString().trim()
-//            val password = passwordEditText.text.toString().trim()
-//
-//            if (username.isEmpty()) {
-//                Toast.makeText(this, "Username tidak boleh kosong", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//
-//            if (password.isEmpty()) {
-//                Toast.makeText(this, "Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//
-//            if (password.length < 6) {
-//                Toast.makeText(this, "Password minimal 6 karakter", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//
-//            // Jika semua valid
-//            Toast.makeText(this, "Login berhasil!", Toast.LENGTH_SHORT).show()
-//
-//            // TODO: Pindah ke halaman berikutnya (main menu, dll)
-//        }
-//    }
-}}
+}
+
+}
