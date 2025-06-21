6a9c8d373973eecad316c36dea2b7be7cc9e7bc2
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2025-06-22 04:07:44 +0700

    menambakan event agar saat klik TextView "Masuk" menuju tampilan login

--- app/src/main/java/com/uti/hidroponikapp/Register_Activity.kt
+++ app/src/main/java/com/uti/hidroponikapp/Register_Activity.kt
@@ -3,6 +3,7 @@ package com.uti.hidroponikapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,8 +15,15 @@ class Register_Activity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         val tombolRegister = findViewById<Button>(R.id.btn_register)
+        val teksLogin = findViewById<TextView>(R.id.tv_login)
 
         tombolRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        teksLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
