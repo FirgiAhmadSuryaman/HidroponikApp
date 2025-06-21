081a1ee6d271ed093b9d84ffd4d03c3b916c12c5
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2025-06-22 04:05:31 +0700

    menambakan event agar saat klik Daftar Akun menuju tampilan login

--- app/src/main/java/com/uti/hidroponikapp/Register_Activity.kt
+++ app/src/main/java/com/uti/hidroponikapp/Register_Activity.kt
@@ -1,6 +1,8 @@
 package com.uti.hidroponikapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,15 @@ class Register_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val tombolRegister = findViewById<Button>(R.id.btn_register)
+
+        tombolRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
